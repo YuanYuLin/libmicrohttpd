301b23c042bb33c7103488a3c8551adce2a5eadc
yyl <yuanyu.lin@gmail.com>
2018-03-28 10:22:24 +0800

    MAJOR_NUM=1 MINOR_NUM=3 AUX_NUM=0 update package

--- Package/CONFIG.py
+++ Package/CONFIG.py
@@ -1,46 +1,109 @@
 import ops
 import iopc
 
-def MAIN_ENV(args):
+pkg_path = ""
+output_dir = ""
+arch = ""
+src_lib_dir = ""
+dst_lib_dir = ""
+src_usr_lib_dir = ""
+dst_lib_dir = ""
+src_usr_bin_dir = ""
+dst_usr_bin_dir = ""
+src_usr_sbin_dir = ""
+dst_usr_sbin_dir = ""
+base_include_dir = ""
+src_include_dir = ""
+dst_include_dir = ""
+
+def set_global(args):
+    global pkg_path
+    global output_dir
+    global arch
+    global src_lib_dir
+    global dst_lib_dir
+    global src_usr_lib_dir
+    global dst_lib_dir
+    global src_usr_bin_dir
+    global dst_usr_bin_dir
+    global src_usr_sbin_dir
+    global dst_usr_sbin_dir
+    global base_include_dir
+    global src_include_dir
+    global dst_include_dir
     pkg_path = args["pkg_path"]
     output_dir = args["output_path"]
+    arch = ops.getEnv("ARCH_ALT")
+    if arch == "armhf":
+        src_lib_dir = iopc.getBaseRootFile("lib/arm-linux-gnueabihf")
+        src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/arm-linux-gnueabihf")
+    elif arch == "armel":
+        src_lib_dir = iopc.getBaseRootFile("lib/arm-linux-gnueabi")
+        src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/arm-linux-gnueabi")
+    elif arch == "x86_64":
+        src_lib_dir = iopc.getBaseRootFile("lib/x86_64-linux-gnu")
+        src_usr_lib_dir = iopc.getBaseRootFile("usr/lib/x86_64-linux-gnu")
+    else:
+        sys.exit(1)
+    dst_lib_dir = ops.path_join(output_dir, "lib")
+
+    src_usr_bin_dir = iopc.getBaseRootFile("usr/bin")
+    dst_usr_bin_dir = ops.path_join(output_dir, "usr/bin")
+
+    src_usr_sbin_dir = iopc.getBaseRootFile("usr/sbin")
+    dst_usr_sbin_dir = ops.path_join(output_dir, "usr/sbin")
+
+    base_include_dir = iopc.getBaseRootFile("usr/include")
+    src_include_dir = ops.path_join(output_dir, "include")
+    dst_include_dir = ops.path_join("include",args["pkg_name"])
+
+def MAIN_ENV(args):
+    set_global(args)
     return False
 
 def MAIN_EXTRACT(args):
-    pkg_dir = args["pkg_path"]
-    output_dir = args["output_path"]
+    set_global(args)
 
-    src_lib = iopc.getBaseRootFile("/usr/lib/arm-linux-gnueabihf/libmicrohttpd.so.10.27.0")
-    ops.copyto(src_lib, output_dir)
+    libmicrohttpd_so = "libmicrohttpd.so.12.39.0"
 
-    ops.ln(output_dir, "libmicrohttpd.so.10.27.0", "libmicrohttpd.so.10")
-    ops.ln(output_dir, "libmicrohttpd.so.10.27.0", "libmicrohttpd.so")
+    ops.mkdir(dst_lib_dir)
+    ops.copyto(ops.path_join(src_usr_lib_dir, libmicrohttpd_so), dst_lib_dir)
+    ops.ln(dst_lib_dir, libmicrohttpd_so, "libmicrohttpd.so.12")
+    ops.ln(dst_lib_dir, libmicrohttpd_so, "libmicrohttpd.so")
+
+    ops.mkdir(src_include_dir)
+    ops.copyto(ops.path_join(base_include_dir, "microhttpd.h"), src_include_dir)
     return False
 
+def MAIN_PATCH(args, patch_group_name):
+    set_global(args)
+    for patch in iopc.get_patch_list(pkg_path, patch_group_name):
+        if iopc.apply_patch(output_dir, patch):
+            continue
+        else:
+            sys.exit(1)
+
+    return True
+
 def MAIN_CONFIGURE(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN_BUILD(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN_INSTALL(args):
-    output_dir = args["output_path"]
+    set_global(args)
 
-    dst_includes = ops.path_join("include",args["pkg_name"])
-
-    src_includes = iopc.getBaseRootFile("/usr/include/microhttpd.h")
-    iopc.installBin(args["pkg_name"], src_includes, dst_includes)
-
-    src_lib = ops.path_join(output_dir, ".")
-    iopc.installBin(args["pkg_name"], src_lib, "usr/lib") 
+    iopc.installBin(args["pkg_name"], ops.path_join(output_dir, "include/."), dst_include_dir)
+    iopc.installBin(args["pkg_name"], ops.path_join(dst_lib_dir, "."), "lib") 
     return False
 
 def MAIN_CLEAN_BUILD(args):
-    output_dir = args["output_path"]
+    set_global(args)
     return False
 
 def MAIN(args):
-    output_dir = args["output_path"]
+    set_global(args)
 
